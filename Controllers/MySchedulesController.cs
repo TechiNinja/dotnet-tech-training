e7c247d820444ce1cfb86c3b41259d018f17de56
Himani Jangid <himani.jangid@intimetec.com>
2026-02-20 19:01:06 +0530

    Separated the participant controller

--- Controllers/MySchedulesController.cs
+++ Controllers/MySchedulesController.cs
@@ -8,32 +8,16 @@ namespace SportsManagementApp.Controllers
     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
-    public class ParticipantController : ControllerBase
+    public class MySchedulesController : ControllerBase
     {
         private readonly IParticipantService _participantService;
 
-        public ParticipantController(IParticipantService participantService)
+        public MySchedulesController(IParticipantService participantService)
         {
             _participantService = participantService;
         }
 
-        [HttpGet("{userId}/events")]
-        public async Task<IActionResult> GetMyEvents(int userId)
-        {
-            var events = await _participantService.GetMyEventsAsync(userId);
-
-            return Ok(events);
-        }
-
-        [HttpGet("{userId}/team")]
-        public async Task<IActionResult> GetMyTeam(int userId)
-        {
-            var teams = await _participantService.GetMyTeamsAsync(userId);
-
-            return Ok(teams);
-        }
-
-        [HttpGet("{userId}/schedule")]
+        [HttpGet("{userId}")]
         public async Task<IActionResult> GetMySchedule(int userId)
         {
             var schedule = await _participantService.GetMySchedulesAsync(userId);
---
+++ Controllers/MyTeamsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SportsManagementApp.Services.Interfaces;
+
+namespace SportsManagementApp.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyTeamsController : ControllerBase
+    {
+        private readonly IParticipantService _participantService;
+
+        public MyTeamsController(IParticipantService participantService)
+        {
+            _participantService = participantService;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetMyTeam(int userId)
+        {
+            var teams = await _participantService.GetMyTeamsAsync(userId);
+
+            return Ok(teams);
+        }
+    }
+}
