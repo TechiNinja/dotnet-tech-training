18215bde54173a35fb18de6e077d95c79d7013cd
darshan-gowda-k-r <darshan.g.kr@intimetec.com>
2026-03-17 09:17:15 +0530

    Registered the Service

--- Extensions/ApplicationServiceExtensions.cs
+++ Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
-﻿using SportsManagementApp.Services.EventRequestService.Implementations;
+﻿using SportsManagementApp.Services;
+using SportsManagementApp.Services.EventRequestService.Implementations;
 using SportsManagementApp.Services.Implementations;
 using SportsManagementApp.Services.Interfaces;
 
@@ -11,6 +12,7 @@ namespace SportsManagementApp.Extensions
             services.AddScoped<IAuthService, AuthService>();
             services.AddScoped<IRoleService, RolesService>();
             services.AddScoped<ISportService, SportService>();
+            services.AddScoped<IEventService, EventService>();
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<ISchedulesService, SchedulesService>();
             services.AddScoped<ITeamsService, TeamsService>();
